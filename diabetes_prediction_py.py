56e457db3b8a0784d08b8d65d6bd78b97b5c335b
Mazen Ahmed <143907458+maldini-3@users.noreply.github.com>
2024-04-13 15:43:34 +0200

    diabetes_prediction_py.py

--- diabetes_prediction_py.py
+++ diabetes_prediction_py.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/11VzadbXbx2F3JZ55OtSEchlWNelQF8fw
 """
 
-!pip install import_ipynb
 
 import streamlit as st
 from importlib import import_module
